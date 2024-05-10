79b496a7eec07f46f6ac66cbf08c1d441b2f1fce
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-05-10 21:37:57 +0300

    home 8

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputElem = document.querySelector("#name-input");
-
-const spanElem = document.querySelector("#name-output");
-
-inputElem.addEventListener("input", (event) => {
-  const value = event.currentTarget.value.trim();
-
-  spanElem.textContent = value || "Anonymous";
-});
